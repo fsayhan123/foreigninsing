4ea8e7b6676832a1632cd7c6c4b96931fc5fbc8e
fsayhan123 <fsayhan123@gmail.com>
2021-07-23 15:06:20 +0800

    P

--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Routes from './routes.js'
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+var firebaseConfig = {
+  apiKey: "AIzaSyD6xnnmm9e4qUOP7w2pDxP2Cbi0FEfjwUU",
+  authDomain: "foreigninsg.firebaseapp.com",
+  projectId: "foreigninsg",
+  storageBucket: "foreigninsg.appspot.com",
+  messagingSenderId: "837171467670",
+  appId: "1:837171467670:web:ba41b72f27f2d9f730b9b7",
+  measurementId: "G-7SM6VENK25"
+};
+
+firebase.initializeApp(firebaseConfig)
+const database = firebase.firestore();
+export default database;
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: Routes
+})
 
 new Vue({
   render: h => h(App),
+  router: router,
 }).$mount('#app')
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import Login from './components/Login.vue'
+import Signup from './components/Signup.vue'
+
+export default [
+    {path : '/', component : Login},
+    {path : '/signup', component : Signup},
+]
